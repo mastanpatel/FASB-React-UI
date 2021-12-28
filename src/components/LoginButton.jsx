9dcb5106cee5c0f2a1685bc3e36e8077e57265f5
Mastan Patel <m.mastanpatel@gmail.com>
2021-12-28 23:59:00 +0530

    implementated oAuth

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function LoginButton() {
-  return <div></div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}></button>;
 }
 
 export default LoginButton;
