d0fb9e2be0be228b7c2eef9ef01b487812e2d251
Mastan Patel <m.mastanpatel@gmail.com>
2021-12-28 10:30:57 +0530

    changes to oAuth

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,181 +1,202 @@
-import React, { useState } from 'react';
-import { sampleProducts } from '../common/sample-products';
-import { MyCommandCell } from './myCommandCell.jsx';
-import { Grid, GridColumn as Column, GridToolbar } from '@progress/kendo-react-grid';
-import { process } from '@progress/kendo-data-query';
-import { sampleInventory } from '../common/sample-inventory';
+import React, { useState } from "react";
+import { sampleProducts } from "../common/sample-products";
+import { MyCommandCell } from "./myCommandCell.jsx";
+import {
+  Grid,
+  GridColumn as Column,
+  GridToolbar,
+} from "@progress/kendo-react-grid";
+import { process } from "@progress/kendo-data-query";
+import { sampleInventory } from "../common/sample-inventory";
 
 const Inventory = (props) => {
-    const editField = "inEdit";
-    const [data, setData] = useState(sampleInventory);
-    const [dataState, setDataState] = useState({ skip: 0, take: 10 })
+  const editField = "inEdit";
+  const [data, setData] = useState(sampleInventory);
+  const [dataState, setDataState] = useState({ skip: 0, take: 10 });
 
-    const generateId = data => data.reduce((acc, current) => Math.max(acc, current.FASBInventoryID), 0) + 1;
+  const generateId = (data) =>
+    data.reduce((acc, current) => Math.max(acc, current.FASBInventoryID), 0) +
+    1;
 
-    const removeItem = (data, item) => {
-        let index = data.findIndex(p => p === item || item.FASBInventoryID && p.FASBInventoryID === item.FASBInventoryID);
-        if (index >= 0) {
-            data.splice(index, 1);
-        }
+  const removeItem = (data, item) => {
+    let index = data.findIndex(
+      (p) =>
+        p === item ||
+        (item.FASBInventoryID && p.FASBInventoryID === item.FASBInventoryID)
+    );
+    if (index >= 0) {
+      data.splice(index, 1);
     }
+  };
 
+  const enterEdit = (dataItem) => {
+    setData(
+      data.map((item) =>
+        item.FASBInventoryID === dataItem.FASBInventoryID
+          ? { ...item, inEdit: true }
+          : item
+      )
+    );
+  };
 
-    const enterEdit = (dataItem) => {
-        setData(data.map(item =>
-            item.FASBInventoryID === dataItem.FASBInventoryID ?
-                { ...item, inEdit: true } : item
-        ));
+  const remove = (dataItem) => {
+    const newData = [...data];
+    removeItem(newData, dataItem);
+    removeItem(sampleInventory, dataItem);
+    setData([...newData]);
+  };
+
+  const add = (dataItem) => {
+    dataItem.inEdit = undefined;
+    dataItem.FASBInventoryID = generateId(sampleInventory);
+
+    sampleInventory.unshift(dataItem);
+    setData([...data]);
+  };
+
+  const discard = (dataItem) => {
+    const newData = [...data];
+    removeItem(newData, dataItem);
+
+    setData(newData);
+  };
+
+  const update = (dataItem) => {
+    const newData = [...data];
+    const updatedItem = { ...dataItem, inEdit: undefined };
+
+    updateItem(newData, updatedItem);
+    updateItem(sampleInventory, updatedItem);
+
+    setData(newData);
+  };
+
+  const cancel = (dataItem) => {
+    const originalItem = sampleInventory.find(
+      (p) => p.FASBInventoryID === dataItem.FASBInventoryID
+    );
+    const newData = data.map((item) =>
+      item.FASBInventoryID === originalItem.FASBInventoryID
+        ? originalItem
+        : item
+    );
+
+    setData(newData);
+  };
+
+  const updateItem = (data, item) => {
+    let index = data.findIndex(
+      (p) =>
+        p === item ||
+        (item.FASBInventoryID && p.FASBInventoryID === item.FASBInventoryID)
+    );
+    if (index >= 0) {
+      data[index] = { ...item };
     }
+  };
 
-    const remove = (dataItem) => {
+  const itemChange = (event) => {
+    const newData = data.map((item) =>
+      item.FASBInventoryID === event.dataItem.FASBInventoryID
+        ? { ...item, [event.field]: event.value }
+        : item
+    );
+    setData(newData);
+  };
 
-        const newData = [...data];
-        removeItem(newData, dataItem);
-        removeItem(sampleInventory, dataItem);
-        setData([...newData]);
-    }
+  const addNew = () => {
+    const newDataItem = { inEdit: true, Discontinued: false };
+    setData([newDataItem, ...data]);
+  };
 
-    const add = (dataItem) => {
-        dataItem.inEdit = undefined;
-        dataItem.FASBInventoryID = generateId(sampleInventory);
+  const cancelCurrentChanges = () => {
+    setData([...sampleInventory]);
+  };
+  let CommandCell = MyCommandCell({
+    edit: enterEdit,
+    remove: remove,
 
-        sampleInventory.unshift(dataItem);
-        setData([...data])
-    }
+    add: add,
+    discard: discard,
 
-    const discard = (dataItem) => {
-        const newData = [...data];
-        removeItem(newData, dataItem);
+    update: update,
+    cancel: cancel,
 
-        setData(newData);
-    }
+    editField: editField,
+  });
+  const hasEditedItem = data.some((p) => p.inEdit);
 
-    const update = (dataItem) => {
-        const newData = [...data]
-        const updatedItem = { ...dataItem, inEdit: undefined };
-
-        updateItem(newData, updatedItem);
-        updateItem(sampleInventory, updatedItem);
-
-        setData(newData);
-    }
-
-    const cancel = (dataItem) => {
-        const originalItem = sampleInventory.find(p => p.FASBInventoryID === dataItem.FASBInventoryID);
-        const newData = data.map(item => item.FASBInventoryID === originalItem.FASBInventoryID ? originalItem : item);
-
-        setData(newData);
-    }
-
-    const updateItem = (data, item) => {
-        let index = data.findIndex(p => p === item || (item.FASBInventoryID && p.FASBInventoryID === item.FASBInventoryID));
-        if (index >= 0) {
-            data[index] = { ...item };
-        }
-    }
-
-    const itemChange = (event) => {
-        const newData = data.map(item =>
-            item.FASBInventoryID === event.dataItem.FASBInventoryID ?
-                { ...item, [event.field]: event.value } : item
-        );
-        setData(newData);
-    }
-
-    const addNew = () => {
-        const newDataItem = { inEdit: true, Discontinued: false };
-        setData([newDataItem, ...data]);
-    }
-
-    const cancelCurrentChanges = () => {
-        setData([...sampleInventory]);
-    }
-    let CommandCell = MyCommandCell({
-        edit: enterEdit,
-        remove: remove,
-
-        add: add,
-        discard: discard,
-
-        update: update,
-        cancel: cancel,
-
-        editField: editField
-    });
-    const hasEditedItem = data.some(p => p.inEdit);
-    return (
-        <div className="container-fluid">
-            <div className='row my-4'>
-                {/* <div className='col-12 col-lg-9 border-right'> */}
-                <Grid
-                    data={process(data, dataState)}
-                    onItemChange={itemChange}
-                    editField={editField}
-                    pageable // uncomment to enable paging
-                    // sortable // uncomment to enable sorting
-                    // filterable // uncomment to enable filtering
-                    onDataStateChange={(e) => setDataState(e.dataState)} // uncomment to enable data operations
-                    {...dataState} // uncomment to enable data operations
-                >
-                    <GridToolbar>
-                        <button
-                            title="Add new"
-                            className="k-button k-primary"
-                            onClick={addNew}
-                        >
-                            Add new
-                        </button>
-                        {hasEditedItem && (
-                            <button
-                                title="Cancel current changes"
-                                className="k-button"
-                                onClick={cancelCurrentChanges}
-                            >
-                                Cancel current changes
-                            </button>
-                        )}
-                    </GridToolbar>
-                    {/* <Column field="ProductID" title="Id" width="50px" editable={false} />
+  return (
+    <div className="container-fluid">
+      <div className="row my-4">
+        {/* <div className='col-12 col-lg-9 border-right'> */}
+        <Grid
+          data={process(data, dataState)}
+          onItemChange={itemChange}
+          editField={editField}
+          pageable // uncomment to enable paging
+          // sortable // uncomment to enable sorting
+          // filterable // uncomment to enable filtering
+          onDataStateChange={(e) => setDataState(e.dataState)} // uncomment to enable data operations
+          {...dataState} // uncomment to enable data operations
+        >
+          <GridToolbar>
+            <button
+              title="Add new"
+              className="k-button k-primary"
+              onClick={addNew}
+            >
+              Add new
+            </button>
+            {hasEditedItem && (
+              <button
+                title="Cancel current changes"
+                className="k-button"
+                onClick={cancelCurrentChanges}
+              >
+                Cancel current changes
+              </button>
+            )}
+          </GridToolbar>
+          {/* <Column field="ProductID" title="Id" width="50px" editable={false} />
                         <Column field="ProductName" title="Product Name" width="250px"/>
                         <Column field="UnitsInStock" title="Units" width="150px" editor="numeric" />
                         <Column field="Discontinued" title="Discontinued" editor="boolean" />
                         <Column cell={CommandCell} width="240px" /> */}
-                    {/* <Column field="FASBInventoryID" title="FASBInventoryID"/>
+          {/* <Column field="FASBInventoryID" title="FASBInventoryID"/>
                         <Column field="QuarterID" title="QuarterID"/>
                         <Column field="QuarterName" title="QuarterName"/>
                         <Column field="StartDate" title="StartDate"/>
                         <Column field="EndDate" title="EndDate"/>
                         <Column field="CCTrackingDealID" title="CCTrackingDealID"/>*/}
-                    <Column field="longDealName" title="Deal Name" width="190px" />
-                    <Column field="CurrentControllingClass" title="Class" width="80px" />
-                    <Column field="CurrentControllingCusip" title="Cusip" />
-                    <Column field="Consolidate" title="Consolidate" width="150px" />
-                    <Column
-                        field="Rules"
-                        title="Edit"
-                        width="150px"
-                        cell={(props) => {
-                            // let field = props.field || "";
-                            return (
-                                <td>
-                                    <button className="k-primary k-button">
-                                        Edit
-                                    </button>
-                                    {/* <input
+          <Column field="longDealName" title="Deal Name" width="190px" />
+          <Column field="CurrentControllingClass" title="Class" width="80px" />
+          <Column field="CurrentControllingCusip" title="Cusip" />
+          <Column field="Consolidate" title="Consolidate" width="150px" />
+          <Column
+            field="Rules"
+            title="Edit"
+            width="150px"
+            cell={(props) => {
+              // let field = props.field || "";
+              return (
+                <td>
+                  <button className="k-primary k-button">Edit</button>
+                  {/* <input
                                         disabled={false}
                                         type="checkbox"
                                         checked={props.dataItem[field]}
                                         /> */}
-                                </td>
-                            );
-                        }}
-                    />
-                    <Column field="CCTrackingComment" title="Comment" width="300px" />
-                    <Column field="CreateTimeStamp" title="CreateTimeStamp" />
-                    <Column field="CreatedBy" title="CreatedBy" />
-                    <Column field="ModifyTimeStamp" title="ModifyTimeStamp" />
-                    <Column field="ModifiedBy" title="ModifiedBy" />
-                    {/* <Column field="DealID" title="DealID"/>
+                </td>
+              );
+            }}
+          />
+          <Column field="CCTrackingComment" title="Comment" width="300px" />
+          <Column field="CreateTimeStamp" title="CreateTimeStamp" />
+          <Column field="CreatedBy" title="CreatedBy" />
+          <Column field="ModifyTimeStamp" title="ModifyTimeStamp" />
+          <Column field="ModifiedBy" title="ModifiedBy" />
+          {/* <Column field="DealID" title="DealID"/>
                         <Column field="longDealName" title="longDealName"/>
                         <Column field="TreppDealName" title="TreppDealName"/>
                         <Column field="DealVintage" title="DealVintage"/>
@@ -238,13 +259,12 @@ const Inventory = (props) => {
                         <Column field="ModifyTimeStamp" title="ModifyTimeStamp"/>
                         <Column field="ModifiedBy" title="ModifiedBy"/>
                         <Column field="CapitalStructurePosition" title="CapitalStructurePosition"/> */}
-
-                </Grid>
-            </div>
-            {/*                
+        </Grid>
+      </div>
+      {/*                
             </div> */}
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Inventory;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Context } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+  <Auth0Context
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Context>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
